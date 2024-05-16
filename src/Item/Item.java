6d5d2edd85d2d508694a5cfeb88feba486204f23
mohkcotS <154826581+mohkcotS@users.noreply.github.com>
2024-05-16 12:17:12 +0700

    Item and item management

---
+++ src/Item/Item.java
@@ -0,0 +1,23 @@
+package Item;
+
+
+import Main.UtilityTool;
+
+import java.awt.image.BufferedImage;
+
+
+public class Item {
+    public String name;
+    public BufferedImage image;
+    public int count = 0;
+    public String rarity;
+    public int price;
+    public boolean caught = false;
+    public int star;
+    public BufferedImage finalImage;
+    public BufferedImage fishingFrame;
+
+
+
+
+}
