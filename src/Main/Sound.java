53b866920b35d7b118c20a953123c14ca6f569e6
mohkcotS <duylm2016@gmail.com>
2024-06-03 23:47:18 +0700

    Add sounds

--- src/Main/Sound.java
+++ src/Main/Sound.java
@@ -31,6 +31,8 @@ public class Sound {
         soundURL[12] = getClass().getResource("/sound/Menu_setting.wav");
         soundURL[13] = getClass().getResource("/sound/selectItem.wav");
         soundURL[14] = getClass().getResource("/sound/clickItem.wav");
+        soundURL[15] = getClass().getResource("/sound/openAndCloseCollection.wav");
+        soundURL[16] = getClass().getResource("/sound/openAndCloseCollection1.wav");
     }
 
     public void setField(int i) {
